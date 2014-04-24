725f0b79ad2d8bec7edf9a7329e0a6051828d8e7
Rafał Krupiński <rafal.krupinski@sorcersoft.com>
2014-04-24 18:16:32 +0200

    remove leading slash (root) from zipped paths

--- src/main/java/org/sorcersoft/sigar/Zip.java
+++ src/main/java/org/sorcersoft/sigar/Zip.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013, 2014 Sorcersoft.com S.A.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.sorcersoft.sigar;
 
 import org.apache.commons.io.FileUtils;
@@ -5,16 +21,8 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
@@ -88,7 +96,7 @@ public class Zip {
     private static void zip(ZipOutputStream targetZip, File root, File zipRoot, FileFilter filter) throws IOException {
         for (File file : root.listFiles(filter)) {
             String fileName = file.getName();
-            String entryPath = new File(zipRoot, fileName).getPath();
+			String entryPath = relative(new File(zipRoot, fileName));
             if (file.isFile()) {
                 log.debug(entryPath);
                 targetZip.putNextEntry(new ZipEntry(entryPath));
@@ -101,5 +109,19 @@ public class Zip {
                 zip(targetZip, file, new File(zipRoot, fileName), filter);
             }
         }
+    }
+
+    private static String relative(File file) {
+        String path = file.getPath();
+        if (!file.isAbsolute()) {
+            return path;
+        }
+
+        for (File root : File.listRoots()) {
+            String rootPath = root.getPath();
+            if (path.startsWith(rootPath))
+                return path.substring(rootPath.length());
+        }
+        throw new IllegalArgumentException(path + " is absolute bot no root matches");
     }
 }
